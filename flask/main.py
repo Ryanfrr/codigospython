8d8d22f32317493fc6eb1756f1168476c250350d
Ryanfrr <ryanfo132007@gmail.com>
2024-11-19 14:37:33 -0400

    arrumando o flask e atividade turtle

--- flask/main.py
+++ flask/main.py
@@ -4,5 +4,5 @@ app = Flask(__name__)
 
 from views import*
 
-if __name__=="__main__":
+if __name__=="__main__":  # se, __name__ ou app for igual a main, ele irá iniciar #todo o codigo que estiver aqui dentro, ou seja, as janelas app. Só podem ser executado. 
     app.run()
--- flask/views.py
+++ flask/views.py
@@ -1,9 +1,9 @@
-from main import app
+from main import app # para ele funcionar, ele precisa importar o app que esta presente no arquivo main
 
 @app.route('/')
 def homepage():
-    return 'meu site no flask'
+    return 'Ola mundo'
 
 @app.route('/blog')
 def blog():
-    return 'bem vindo ao boga'
+    return 'bem vindo ao blog'
