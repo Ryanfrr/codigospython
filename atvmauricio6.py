38fe56e39ba02a827255b3a72026968eb3b1854e
Ryanfrr <ryanfo132007@gmail.com>
2024-11-21 10:46:09 -0400

    lista1

---
+++ atvmauricio6.py
@@ -0,0 +1,10 @@
+def soma_lista(lista):
+    lista = [lista]
+    list(lista)
+    
+
+num1 = int(input('Escreva um número: '))
+num2 = int(input('Escreva um número: '))
+num3 = int(input('Escreva um número: '))
+res = soma_lista(num1)
+print("Resposta: ",res)
---
+++ atvmauricio9.py
@@ -0,0 +1,12 @@
+
+def fatorial (n,n1):
+    n = 1 
+    while (n1 > 1):  
+        n = n * n1 
+        n1 = n1 - 1    
+        print('O fatorial é {}.'.format(fatorial))
+valor = int(input('Entre com um número para saber o fatorial:')) 
+fator = (0)
+res = (fatorial(fator,valor))
+print("Fatorial: ",res)
+
